8927b98ebb6d3cfa2ec4f4e0df7bd2a266964ae5
Hansanca <hansanca@gmail.com>
2022-08-11 15:50:22 -0400

    placeholder for endpoints built, server ready

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,33 @@
 // Write your "actions" router here!
+
+const express = require("express");
+
+const {
+    get,
+    insert,
+    update,
+    remove,
+} = require('./actions-middlware');
+
+const router = express.Router();
+
+
+router.get('/api/actions', (req, res, next) => {
+
+})
+
+router.get('/api/actions/:id', (req, res, next) => {
+
+})
+
+router.post('/api/actions', (req, res, next) => {
+
+})
+
+router.put('/api/actions/:id', (req, res, next) => {
+
+})
+
+router.delete('/api/actions/:id', (req, res, next) => {
+
+})
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,38 @@
 // Write your "projects" router here!
+
+const express = require("express");
+
+const {
+  get,
+  insert,
+  update,
+  remove,
+  getProjectActions,
+} = require("./projects-middleware");
+
+const router = express.Router();
+
+
+router.get('/api/projects', (req, res, next) => {
+
+})
+
+router.get('/api/projects/:id', (req, res, next) => {
+
+})
+
+router.post('/api/projects', (req, res, next) => {
+
+})
+
+router.put('/api/projects/:id', (req, res, next) => {
+
+})
+
+router.delete('/api/projects/:id', (req, res, next) => {
+
+})
+
+router.get('/api/projects/:id/actions', (req, res, next) => {
+
+})
